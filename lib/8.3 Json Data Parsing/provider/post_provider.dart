9c69ee41d809598acca9f5399e5a44c141508b20
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-07-26 20:42:07 +0530

    Update post_provider.dart

--- lib/8.3 Json Data Parsing/provider/post_provider.dart
+++ lib/8.3 Json Data Parsing/provider/post_provider.dart
@@ -1,19 +1,21 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:json/8.3%20Json%20Data%20Parsing/model/post.dart';
 
-class PostsProvider extends ChangeNotifier {
+import '../model/post.dart';
+
+class PostsProvider extends ChangeNotifier{
+
   late PostsModal postsModal;
 
   Future<void> jsonParsing() async {
-    String json = await rootBundle.loadString('assets/json/posts.json');
+    String json = await rootBundle.loadString('assets/json/photos.json');
     final posts = jsonDecode(json);
     postsModal = PostsModal.fromJson(posts);
     notifyListeners();
   }
 
-  PostsProvider() {
+  PostsProvider(){
     jsonParsing();
   }
 }
